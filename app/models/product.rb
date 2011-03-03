e8fc9f6ee75ec3f8e0e5aab501d3d7f56746c697
byronurbina <byron_urbina5@hotmail.com>
2011-03-03 16:20:01 -0600

    callbacks terminados

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,40 @@
 class Product < ActiveRecord::Base
-validates_presence_of :name, :title, :description, :price, :image_url 
+belongs_to: inventories
+
+validates_presence_of :title, :description, :price, :image_url 
 validates_numericality_of :price, :greater_than => 0.01 
 validates_numericality_of :proyection, :greater_than => 0 
 validates_uniqueness_of :title
 validates_format_of :image_url, :with => %r{\.(gif|jpg|png)$}i,:message => "check URL"
 
+before_create : metodo1
+after_update : metodo2
+after_destroy : metodo3
+
+
+	def metodo1
+		invent = Inventory.new
+		invent.previous_balance = 0
+		invent.new_balance = ammount
+		invent.future_balance = ammount + projection
+		invent.save
+	end
+	
+	def metodo2
+		anterior = regAnt =Inventory.where("product_id = :id").last
+		nuevo = Inventory.new
+		nuevo.previous_balance = anterior.new_balance
+		nuevo.new_balance = ammount
+		nuevo.future_balance = ammount + projection
+		nuevo.save
+	end
+	
+	def metodo3
+		consulta = Inventory.find_by_product_id(id)
+		consulta.each do |tupla|
+		tupla.delete()
+		end
+	end
+		
 
 end
